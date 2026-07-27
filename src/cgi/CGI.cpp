dfdcc74ef9094048c49d03db27760094111fa3f5
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-07-27 02:05:07 +0200

    feat: added createSubprocess() to CGI

--- src/cgi/CGI.cpp
+++ src/cgi/CGI.cpp
@@ -34,5 +34,31 @@ void CGI::processInput(std::string input)
 
 void CGI::createSubprocess(const std::string& filepath, const std::string& input)
 {
+	// checks should be done upstream
+	pid_t pid;
 
+	pid = fork();
+	if (pid == -1)
+		throw std::runtime_error("Forking process failed.");
+
+	if (pid == 0)
+	{
+		// Child process
+		char *args[] = {const_cast<char *>(filepath.c_str()), const_cast<char *>(input.c_str()), NULL};
+
+		execve(args[0], args, NULL); // envp useless?
+		// still in child process, execve failed
+		// TODO: properly handle error
+		throw std::runtime_error("Execution of CGI script failed.");
+	}
+	else
+	{
+		// Parent process
+		int status;
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			std::cout << "CGI script exited with status: " << WEXITSTATUS(status) << std::endl;
+		else
+			std::cout << "CGI script did not exit normally." << std::endl;
+	}
 }
