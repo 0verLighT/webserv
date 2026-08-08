7e2d4fe97f456519938f5b6ea743eafddc239e85
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-08-08 03:46:56 +0200

    fix: restore behaviour to pre-merge

--- mk/sources.mk
+++ mk/sources.mk
@@ -1,11 +1,5 @@
 SOURCES = \
 	src/main.cpp \
-	src/Server.cpp \
-	src/Client.cpp \
-	src/HttpRequest.cpp \
-	src/Logger.cpp \
-	src/HttpResponse.cpp \
-	src/RequestHandler.cpp \
 	src/utils.cpp \
 	src/TOMLParser.cpp \
 	src/CGI.cpp
